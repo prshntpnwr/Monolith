0a94a26f7ae4cbc54192e8703ceeb155c7ce0300
Prashant Panwar <prashant.panwar777@gmail.com>
2017-03-05 22:50:24 +0530

    refactor: changes in volley request

--- app/src/main/java/com/example/prashant/monolith/fragments/GalleryFragment.java
+++ app/src/main/java/com/example/prashant/monolith/fragments/GalleryFragment.java
@@ -1,6 +1,5 @@
 package com.example.prashant.monolith.fragments;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,15 +12,11 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.Gson;
-
 import com.example.prashant.monolith.R;
 import com.example.prashant.monolith.adapters.GalleryAdapter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
@@ -45,90 +40,34 @@ public class GalleryFragment extends Fragment {
 
     @Override
     public void onStart() {
-        new ImageLoadTask().execute();
+        ImageLoadTask();
         super.onStart();
     }
 
-    public class ImageLoadTask extends AsyncTask<Void, Void, ArrayList<String>> {
+    public void ImageLoadTask() {
 
-        private final String LOG_TAG = ImageLoadTask.class.getSimpleName();
+        String url = "https://api.nasa.gov/planetary/apod?date=2017-03-04&hd=True&api_key=DEMO_KEY";
 
-        String response = null;
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-        @Override
-        protected void onPostExecute(ArrayList<String> result) {
-            GalleryAdapter adapter = new GalleryAdapter(getActivity(), result);
-            gridView.setAdapter(adapter);
-        }
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("here goes response", response.toString());
+                        String date  = null;
+                        try {
+                            date = response.getString("date");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d("----------", "date is : " + date);
+                    }
+                }, new Response.ErrorListener() {
 
-        @Override
-        protected ArrayList<String> doInBackground(Void... params) {
-            try {
-                Log.d(LOG_TAG, "Starting fetch");
-                // Create a new RestTemplate instance
-//                RestTemplate restTemplate = new RestTemplate();
-//
-//                // Add the String message converter
-//                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-//
-//                // Make the HTTP GET request, marshaling the response to a String
-//
-//                Gson gson = new Gson();
-//
-//                String url = "https://api.nasa.gov/planetary/apod?date=2017-03-04&hd=True&api_key=DEMO_KEY";
-//
-//                response = restTemplate.getForObject(url, String.class);
-//
-//                Log.i("response is {}", gson.toJson(response));
-//                Log.d(LOG_TAG, "Finished fetch");
-
-                String url = "https://api.nasa.gov/planetary/apod?date=2017-03-04&hd=True&api_key=DEMO_KEY";
-
-                JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                String date  = null;
-                                try {
-                                    date = response.getString("date");
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                                Log.d(LOG_TAG, "date is : " + date);
-                            }
-                        }, new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                error.printStackTrace();
-                            }
-                        });
-
-//                try {
-//                    JSONObject object = new JSONObject(response);
-//
-//                    String date  = object.getString("date");
-//                    Log.d(LOG_TAG, "date is : " + date);
-//
-//                    String detail = object.getString("explanation");
-//                    Log.d(LOG_TAG, "explanation is : " + detail);
-//
-//                    String image_path = object.getString("url");
-//                    Log.d(LOG_TAG, "image url is : " + image_path);
-//
-//                    String title = object.getString("title");
-//                    Log.d(LOG_TAG, "title is : " + title);
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Error", e);
-                return null;
-            }
-            return null;
-        }
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
     }
 }
