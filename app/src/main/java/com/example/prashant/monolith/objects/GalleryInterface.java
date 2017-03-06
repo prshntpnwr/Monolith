b7be11ffee6139ae2874c58317f591c347d95fab
Prashant Panwar <prashant.panwar777@gmail.com>
2017-03-06 19:09:59 +0530

    refactor: changes in interface

---
+++ app/src/main/java/com/example/prashant/monolith/objects/GalleryInterface.java
@@ -0,0 +1,6 @@
+package com.example.prashant.monolith.objects;
+
+public interface GalleryInterface {
+
+    String API_BASE_URL = "https://api.nasa.gov/planetary/apod?date=2017-03-04&hd=True&api_key=DEMO_KEY";
+}
