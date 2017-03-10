7cbdc24a0cea8d91cbd02474069498ff7e64c99c
Prashant Panwar <prashant.panwar777@gmail.com>
2017-03-10 18:44:51 +0530

    refactor: remove unuse code

--- app/src/main/java/com/example/prashant/monolith/ImageFragment.java
+++ app/src/main/java/com/example/prashant/monolith/ImageFragment.java
@@ -64,22 +64,13 @@ public class ImageFragment extends Fragment{
 
     private void setupToolbar() {
         if (toolbar != null) {
-//
-//            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-//            ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//            ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-//            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {/home/prashant/Downloads/Cinephile-master/app/src/main/res/drawable-hdpi/ic_back.png
-//                    ((AppCompatActivity) getActivity()).supportFinishAfterTransition();
-//                }
-//            });
 
             toolbar.setNavigationIcon(ContextCompat.getDrawable(getActivity(), R.drawable.ic_back));
             toolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     getActivity().finish();
+
                 }
             });
 
