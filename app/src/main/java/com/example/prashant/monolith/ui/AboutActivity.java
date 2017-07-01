cfc3bd67450a015dfd7f4044531a940ad9d55aa4
Prashant Panwar <prashant.panwar777@gmail.com>
2017-07-02 00:00:01 +0530

    feat: add about activity

---
+++ app/src/main/java/com/example/prashant/monolith/ui/AboutActivity.java
@@ -0,0 +1,41 @@
+package com.example.prashant.monolith.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.prashant.monolith.R;
+import com.vansuita.materialabout.builder.AboutBuilder;
+import com.vansuita.materialabout.views.AboutView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AboutView view = AboutBuilder.with(this)
+                .setPhoto(R.mipmap.profile_picture)
+                .setCover(R.mipmap.profile_cover)
+                .setName("Your Full Name")
+                .setSubTitle("Mobile Developer")
+                .setBrief("I'm warmed of mobile technologies. Ideas maker, curious and nature lover.")
+                .setAppIcon(R.mipmap.ic_launcher)
+                .setAppName(R.string.app_name)
+                .addGooglePlayStoreLink("8002078663318221363")
+                .addGitHubLink("user")
+                .addFacebookLink("user")
+                .addFiveStarsAction()
+                .setVersionNameAsAppSubTitle()
+                .addShareAction(R.string.app_name)
+                .setWrapScrollView(true)
+                .setLinksAnimated(true)
+                .setShowAsCard(true)
+                .build();
+
+        setContentView(view);
+        
+        this.setTitle("About");
+
+//        addContentView(view, );
+    }
+}
